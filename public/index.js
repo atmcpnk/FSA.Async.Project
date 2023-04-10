bcecd6b00d2376787cf9dda9f8288347bf86403d
abrahamflores <a.flrs182@gmail.com>
2023-04-10 13:03:22 -0700

    starting site, issues with webpack server

---
+++ public/index.js
@@ -0,0 +1,8 @@
+// entry file
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>Hello, world!</div>,
+  document.getElementById('root') // make sure this is the same as the id of the div in your public/index.html
+);
